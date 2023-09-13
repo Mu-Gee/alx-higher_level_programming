d43bfc28a0088b3df93f8a071267ca6c645138f7
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2023-09-13 20:47:54 +0300

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,7 +5,9 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => myObject.value++;
+function incr(myObject) {
+  myObject.value++;
+}
 
 myObject.incr();
 console.log(myObject);
