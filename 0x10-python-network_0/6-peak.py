e4f8c07d27a453314e9e59596fb2629240a33640
Mu-Gee <jamesjim908@gmail.com>
2023-12-01 17:31:17 +0300

    haha made it worse

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,29 +3,28 @@
 find the peak in an unordered list of
 integers
 """
-from typing import List, Optional
 
-def find_peak(list_of_integers: List[int]) -> Optional[int]:
+
+def find_peak(list_of_integers):
     """
-    Find the peak in a list of integers.
-    
+    find the peak in a list of intergers
     Args:
-        list_of_integers (List[int]): List of integers.
-        
-    Returns:
-        Optional[int]: The peak integer(s).
+        list_of_integers(list) -> List of intergers
+    Return:
+        int(the peak integer(s))
     """
-    if not list_of_integers:
+    if len(list_of_integers) == 0:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
 
-    low, high = 0, len(list_of_integers) - 1
-
-    while low < high:
-        mid = (low + high) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-
-    return list_of_integers[low]
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
